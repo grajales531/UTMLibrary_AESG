c6a97ba9779da6b9edfb20d488bd9d93fc545ad4
Grajales <grajalesangel531@gmail.com>
2026-02-08 18:37:10 -0600

    Initial commit

---
+++ subcarpeta/hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Hello
+{
+    public static void Main(string[] args)
+    {
+        DisplayWelcomeMessage();
+    }
+
+    public static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("¡Bienvenido!");
+    }
+}
